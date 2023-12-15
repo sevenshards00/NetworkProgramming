4ef65f9098626034b6a08179b86be55f5068b725
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-15 17:35:13 +0900

    연결지향형 소켓(TCP 소켓) 프로그래밍 예제 - Windows, MBCS기반

---
+++ Windows/Ch02-SocketType&Protocol/tcp_client_win/tcp_client_win/tcp_client_win.c
@@ -0,0 +1,79 @@
+﻿/*
+* TCP/IP Socket Programming - 소켓의 타입과 프로토콜의 설정
+* 파일명: tcp_client_win.c
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-15
+* 이전 버전 작성 일자:
+* 버전 내용: 연결지향형 소켓(TCP 소켓) 프로그래밍 예제 - Client, Windows, MBCS기반
+* 이전 버전 내용:
+*/
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char message[30];
+	int strLen = 0;
+	int idx = 0, readLen = 0;
+	if (argc != 3)
+	{
+		printf("Usage: %s <IP> <Port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("socket() error!");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	// servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	// 현재 iner_addr은 deprecated되어 곧 쓰지 않을 수 있음
+	// 따라서 inet_pton() 또는 InetPton()을 사용할 것을 권장 -> header파일로 WS2tcpip.h 추가 필요
+	// pton == pc to network / ntop == network to pc
+	// #define _WINSOCK_DEPRECATED_NO_WARNINGS를 사용해도 되지만 여기서는 권장하는 방법을 사용해보려고 함
+	// 반환값은 정수형
+	inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	// 리눅스에서의 connect
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error!");
+
+	while (readLen = recv(hSocket, &message[idx++], 1, 0))
+	{
+		if (strLen == -1)
+			ErrorHandling("recv() error!");
+
+		strLen += readLen;
+	}	
+
+	printf("Message from server: %s\n", message);
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+}
+
+
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ Windows/Ch02-SocketType&Protocol/tcp_server_win/tcp_server_win/tcp_server_win.c
@@ -0,0 +1,70 @@
+﻿/*
+* TCP/IP Socket Programming - 소켓의 타입과 프로토콜의 설정
+* 파일명: tcp_server_win.c
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-15
+* 이전 버전 작성 일자:
+* 버전 내용: 연결지향형 소켓(TCP 소켓) 프로그래밍 예제 - Server, Windows, MBCS기반
+* 이전 버전 내용:
+*/
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData; // 큰 의미는 없으나 사용해야 함
+	SOCKET hServSock, hClntSock; // 소켓 핸들 (리눅스의 파일 디스크립터)
+	SOCKADDR_IN servAddr, clntAddr;
+
+	int szClntAddr;
+	char message[] = "Hello World!";
+	if (argc != 2)
+	{
+		printf("Usage: %s <Port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) // WinSock 라이브러리 초기화, 소켓 버전 2.2
+		ErrorHandling("WSAStartup() error!");
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0); // 리눅스의 socket 함수와 동일, 소켓 (핸들)생성
+	if (hServSock == INVALID_SOCKET)
+		ErrorHandling("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) // 리눅스의 bind와 동일, 소켓에 주소 정보 할당
+		ErrorHandling("bind() error");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR) // 리눅스의 listen과 동일, 소켓 통신 대기 상태
+		ErrorHandling("listen() error");
+
+	szClntAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr); // 소켓 통신 수락
+	if (hClntSock == INVALID_SOCKET)
+		ErrorHandling("accept() error");
+
+	send(hClntSock, message, sizeof(message), 0);
+	closesocket(hClntSock); // 리눅스에서는 파일과 소켓이 동일하므로 close
+	closesocket(hServSock); // Windows에서는 파일과 소켓은 다르므로 closesocket이 따로 있음. CloseHandle과 비슷하다고 보면 될 듯.
+	WSACleanup(); // WinSock 라이브러리 해제, OS에 반환
+
+	return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
