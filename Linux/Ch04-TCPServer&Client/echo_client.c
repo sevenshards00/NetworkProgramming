dfcdb543dea0e964801f22a7945cb9a4229e1178
sevenshards00 <sevenshards@hanyang.ac.kr>
2023-12-17 16:32:22 +0900

    주석 추가

--- Linux/Ch04-TCPServer&Client/echo_client.c
+++ Linux/Ch04-TCPServer&Client/echo_client.c
@@ -32,15 +32,18 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 	
+	// 클라이언트의 소켓 생성
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(sock == -1)
 		error_handling("socket() error");
 	
+	// 입력받은 값으로 sockaddr_in 구조체의 값을 채움 -> 서버의 주소를 설정
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	inet_aton(argv[1], &serv_addr.sin_addr);
 	serv_addr.sin_port = htons(atoi(argv[2]));
 	
+	// 서버로 연결 요청(connect), 연결 요청이 성공하면 클라이언트 소켓에 주소가 할당된다.
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error");
 	else
@@ -53,9 +56,12 @@ int main(int argc, char* argv[])
 		
 		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
 			break;
-		
+		// 현 시점에서 문제가 되는 부분
+		// TCP 통신의 특성은 데이터의 경계가 없다는 것.
+		// 지금 코드는 패킷이 나눠져서 보내는 경우를 전혀 고려하지 않은 상
 		write(sock, message, strlen(message));
 		str_len = read(sock, message, BUF_SIZE-1);
+		// 현 시점에서 문제가 되는 부분
 		message[str_len] = 0;
 		printf("Message from server: %s" , message);
 	}
