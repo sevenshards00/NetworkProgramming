1b270957cc7eef625381092ddd14cd255f6edd5f
sevenshards00 <sevenshards@hanyang.ac.kr>
2023-12-20 21:35:44 +0900

    Linux 기반 시그널 핸들링 예제 - sigaction

--- Linux/Ch10-Multi-ProcessServer/sigaction.c
+++ Linux/Ch10-Multi-ProcessServer/sigaction.c
@@ -1,11 +1,11 @@
 /*
 * TCP/IP Socket Programming - 멀티프로세스 기반의 서버 구현
-* 파일명: signal.c
+* 파일명: sigaction.c
 * 파일 버전: 0.1
 * 작성자: Sevenshards
 * 작성 일자: 2023-12-20
 * 이전 버전 작성 일자:
-* 버전 내용: Linux 기반 시그널 핸들링 예제 - signal, alarm
+* 버전 내용: Linux 기반 시그널 핸들링 예제 - sigaction
 * 이전 버전 내용:
 */
 
@@ -20,21 +20,14 @@ void timeout(int sig)
     alarm(2);
 }
 
-void keycontrol(int sig)
-{
-    if(sig == SIGINT)
-        puts("CTRL+C Pressed");
-}
-
 int main(int argc, char* argv[])
 {
     int i;
-    // void (*signal(int signo, void(*func)(int))(int));
-    // 함수의 이름이 signal
-    // 인자는 int signo, void(*func)(int)를 받는다.
-    // 반환형은 '매개변수형이 int이고 반환형이 void인 함수 포인터'를 반환
-    signal(SIGALRM, timeout);
-    signal(SIGINT, keycontrol);
+    struct sigaction act;
+    act.sa_handler = timeout;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGALRM, &act, 0);
     alarm(2);
 
     for(i=0; i<3; i++)
